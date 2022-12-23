345a602091818274e67813fff381d0c713b8502b
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-12-23 16:20:00 -0300

    Fix test description

--- src/week1/4-sparseArrays/sparseArrays.test.ts
+++ src/week1/4-sparseArrays/sparseArrays.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test} from '@jest/globals'
 import sparseArrays from './sparseArrays'
 
 describe('sparse arrays function', () => {
-  test('Presents the result for morning time', () => {
+  test('Presents the result one single string on the array and matching query', () => {
     const strings = ['a']
     const queries = ['a']
     const output = [1]
@@ -12,7 +12,7 @@ describe('sparse arrays function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for two strings with the same value on the array and matching query', () => {
     const strings = ['a', 'a']
     const queries = ['a']
     const output = [2]
@@ -22,7 +22,7 @@ describe('sparse arrays function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for two strings and only one query', () => {
     const strings = ['a', 'b']
     const queries = ['a']
     const output = [1]
@@ -32,7 +32,7 @@ describe('sparse arrays function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for two strings and two queries that matches', () => {
     const strings = ['a', 'b']
     const queries = ['a', 'b']
     const output = [1, 1]
@@ -42,7 +42,7 @@ describe('sparse arrays function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for three strings and two queries', () => {
     const strings = ['a', 'b', 'c']
     const queries = ['a', 'b']
     const output = [1, 1]
@@ -52,20 +52,10 @@ describe('sparse arrays function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for more complex strings and queries', () => {
     const strings = ['ab', 'ab', 'abc']
     const queries = ['ab', 'abc', 'bc']
     const output = [2, 1, 0]
-
-    const result = sparseArrays(strings, queries)
-
-    expect(result).toStrictEqual(output)
-  })
-
-  test('Presents the result for morning time', () => {
-    const strings = ['def', 'de', 'fgh']
-    const queries = ['de', 'lmn', 'fgh']
-    const output = [1, 0, 1]
 
     const result = sparseArrays(strings, queries)
 
--- src/week1/5-lonelyInteger/lonelyInteger.test.ts
+++ src/week1/5-lonelyInteger/lonelyInteger.test.ts
@@ -1,8 +1,8 @@
 import {describe, expect, test} from '@jest/globals'
 import lonelyInteger from './lonelyInteger'
 
-describe('lonely time function', () => {
-  test('Presents the result for morning time', () => {
+describe('lonely integer function', () => {
+  test('Presents the result for one single element on the array', () => {
     const input = [1]
     const output = 1
 
@@ -11,7 +11,7 @@ describe('lonely time function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for two matching elements on the array', () => {
     const input = [1, 1]
     const output = 0
 
@@ -20,7 +20,7 @@ describe('lonely time function', () => {
     expect(result).toStrictEqual(output)
   })
 
-  test('Presents the result for morning time', () => {
+  test('Presents the result for two matching elements and one single different one on the array', () => {
     const input = [1, 1, 2]
     const output = 2
 
